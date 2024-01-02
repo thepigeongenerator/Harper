712ce8d96f0d0860f90ffadd4fabde16fcdada39
QuinnBitz <quinnthepigeon@gmail.com>
2024-01-02 15:59:02 +0100

    fix:already_responded_with_ping

--- MinecraftServerApplication/Discord/Commands/UtilCommands.cs
+++ MinecraftServerApplication/Discord/Commands/UtilCommands.cs
@@ -4,6 +4,6 @@ namespace MinecraftServerApplication.Discord.Commands;
 internal class UtilCommands : CommandHandler {
     [SlashCommand("ping", "generic command which responds with 'pong'")]
     public async Task PingCmd() {
-        await RespondAsync("pong!");
+        await SetInfo("pong!");
     }
 }
