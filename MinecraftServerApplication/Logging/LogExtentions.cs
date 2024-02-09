b43e60fe617b995e530a0f9c878e0aa30589cd48
QuinnBitz <quinnthepigeon@gmail.com>
2024-02-09 22:01:04 +0100

    feat:add_agressive_inlining

--- MinecraftServerApplication/Logging/LogExtentions.cs
+++ MinecraftServerApplication/Logging/LogExtentions.cs
@@ -1,11 +1,12 @@
 ﻿using log4net;
 using System.Diagnostics;
+using System.Runtime.CompilerServices;
 
 namespace MinecraftServerApplication.Logging;
 internal static class LogExtentions {
-    public static void LogInfo(this IModule module, string msg, params object[] args) => LogManager.GetLogger(module.GetType().Name).Info(string.Format(msg, args));
-    public static void LogWarn(this IModule module, string msg, params object[] args) => LogManager.GetLogger(module.GetType().Name).Warn(string.Format(msg, args));
-    public static void LogError(this IModule module, string msg, params object[] args) => LogManager.GetLogger(module.GetType().Name).Error(string.Format(msg, args));
-    public static void LogFatal(this IModule module, string msg, params object[] args) => LogManager.GetLogger(module.GetType().Name).Fatal(string.Format(msg, args));
-    public static void LogDebug(this IModule module, string msg, params object[] args) => LogManager.GetLogger(module.GetType().Name).Debug(string.Format(msg, args));
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static void LogInfo(this IModule module, string msg, params object[] args) => LogManager.GetLogger(module.GetType().Name).Info(string.Format(msg, args));
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static void LogWarn(this IModule module, string msg, params object[] args) => LogManager.GetLogger(module.GetType().Name).Warn(string.Format(msg, args));
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static void LogError(this IModule module, string msg, params object[] args) => LogManager.GetLogger(module.GetType().Name).Error(string.Format(msg, args));
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static void LogFatal(this IModule module, string msg, params object[] args) => LogManager.GetLogger(module.GetType().Name).Fatal(string.Format(msg, args));
+    [MethodImpl(MethodImplOptions.AggressiveInlining)] public static void LogDebug(this IModule module, string msg, params object[] args) => LogManager.GetLogger(module.GetType().Name).Debug(string.Format(msg, args));
 }
