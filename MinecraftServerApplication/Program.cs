0c70d6a4b2cdd41ac3e17d8102090fcb21db0889
Quinn <99677023+thepigeongenerator@users.noreply.github.com>
2024-06-15 21:59:08 +0200

    v5.15

--- MinecraftServerApplication/Program.cs
+++ MinecraftServerApplication/Program.cs
@@ -16,7 +16,7 @@ internal static class Program
     #else
     public const string SETTINGS_PATH = "/etc/harper";
     #endif
-    public static readonly Version _appVersion = new(5, 14);
+    public static readonly Version _appVersion = new(5, 15);
     private static readonly ILog _log = LogManager.GetLogger("System");
     private static readonly ManualResetEvent shutdownEvent = new(false);
     private static readonly List<IModule> _modules = [];
