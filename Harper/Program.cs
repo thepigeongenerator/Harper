3a4282c6aa33be0760580d5b08cf9fb98a192aee
unset <quinnthepigeon@gmail.com>
2024-12-27 21:52:59 +0100

    remove restarting, we'll just manage this outside the code

--- Harper/Program.cs
+++ Harper/Program.cs
@@ -1,11 +1,13 @@
+using System;
+using System.Diagnostics;
+using Harper.Util;
+
 int8 exitCode = 1;
 
-do
 {
     using Harper.Core application = new();
     application.Run();
     exitCode = application.ExitCode;
 }
-while (exitCode == 2);
 
 return unchecked((uint8)(exitCode));
