5d74e3465a23d08dd6da73907ad0d84961e8f37a
unset <quinnthepigeon@gmail.com>
2025-03-04 22:27:35 +0100

    write errors to stdout

--- Harper/Logging/Log.cs
+++ Harper/Logging/Log.cs
@@ -1,11 +1,8 @@
-using System;
 using log4net;
 using log4net.Appender;
 using log4net.Config;
 using log4net.Core;
-using log4net.Filter;
 using log4net.Layout;
-using log4net.Repository;
 using log4net.Repository.Hierarchy;
 
 namespace Harper.Logging;
@@ -30,28 +27,31 @@ public static class Log
         PatternLayout pattern = new("[%date{HH:mm:ss}] %-8level %-32.32logger{1} - %message%newline");
         pattern.ActivateOptions();
 
-        // create the console appender with the pattern layout
-        AnsiColorTerminalAppender console = new()
+        // create the console appender printing to stdout
+        AnsiColorTerminalAppender stdout = new()
         {
-            Layout = pattern
+            Layout = pattern,
+            Target = "Console.Out",
         };
+        stdout.AddMapping(new() { Level = Level.Debug, ForeColor = AnsiColorTerminalAppender.AnsiColor.Magenta });
+        stdout.AddMapping(new() { Level = Level.Info, ForeColor = AnsiColorTerminalAppender.AnsiColor.White });
+        stdout.AddMapping(new() { Level = Level.Warn, ForeColor = AnsiColorTerminalAppender.AnsiColor.Yellow });
+        stdout.ActivateOptions();
 
-        AnsiColorTerminalAppender.LevelColors[] colours = [
-            new() { Level = Level.Debug, ForeColor = AnsiColorTerminalAppender.AnsiColor.Magenta },
-            new() { Level = Level.Info, ForeColor = AnsiColorTerminalAppender.AnsiColor.White },
-            new() { Level = Level.Warn, ForeColor = AnsiColorTerminalAppender.AnsiColor.Yellow },
-            new() { Level = Level.Error, ForeColor = AnsiColorTerminalAppender.AnsiColor.Red },
-            new() { Level = Level.Fatal, ForeColor = AnsiColorTerminalAppender.AnsiColor.White, BackColor = AnsiColorTerminalAppender.AnsiColor.Red },
-        ];
-
-        foreach (AnsiColorTerminalAppender.LevelColors mapping in colours)
-            console.AddMapping(mapping);
-
-        console.ActivateOptions();
+        // create a console appender printing to stderr
+        AnsiColorTerminalAppender stderr = new()
+        {
+            Layout = pattern,
+            Target = "Console.Error",
+        };
+        stderr.AddMapping(new() { Level = Level.Error, ForeColor = AnsiColorTerminalAppender.AnsiColor.Red });
+        stderr.AddMapping(new() { Level = Level.Fatal, ForeColor = AnsiColorTerminalAppender.AnsiColor.White, BackColor = AnsiColorTerminalAppender.AnsiColor.Red });
+        stdout.ActivateOptions();
 
         // configure the root logger with a treshold
         var hierarchy = (Hierarchy)LogManager.GetRepository();
-        hierarchy.Root.AddAppender(console);
+        hierarchy.Root.AddAppender(stdout);
+        hierarchy.Root.AddAppender(stderr);
         hierarchy.Configured = true;
 
         if (LogDebug())
@@ -59,8 +59,8 @@ public static class Log
         else
             hierarchy.Root.Level = log4net.Core.Level.Info;
 
-        // configure the repository with the appender
-        BasicConfigurator.Configure(console);
+        // configure the repository with the appenders
+        BasicConfigurator.Configure(stdout, stderr);
     }
 
     public static ILog GetLogger(this object obj) => LogManager.GetLogger(obj.GetType());
